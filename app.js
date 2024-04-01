80aa42ccf75eae1e3de6db925667d5384f67feb4
rufusshakin300 <rufuskinn7@gmail.com>
2024-04-01 15:34:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const list = document.querySelector('#movie-list ul');
+    const forms = document.forms;
+    //delete movie
+    list.addEventListener('click',(e)=>{
+        if(e.target.className=="delete"){
+            const li = e.target.parentElement;
+            li.parentNode.removeChild(li);
+        }
+    });
+
+     //add movie
+     const addForm = forms['add-movie'];
+     addForm.addEventListener('submit', function (e) {
+         e.preventDefault(); //corrected function name
+         //create the ele's
+         const value = addForm.querySelector('input[type="text"]').value;
+         const li = document.createElement('li');
+         const movieName = document.createElement('span');
+         const deleteBtn = document.createElement('span');
+         //add text context
+         movieName.textContent = value;
+         deleteBtn.textContent = 'Delete';
+         //add classes
+         movieName.classList.add('name');
+         deleteBtn.classList.add('delete');
+         //append Dom
+         li.appendChild(movieName);
+         li.appendChild(deleteBtn);
+         list.appendChild(li); //corrected parent element
+     });
+});
+
+
